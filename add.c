d345457d5d6b27c0cf4a3a4ce67b17971abda40b
ED-DEROUACH <97236123+ED-DEROUACH@users.noreply.github.com>
2023-08-23 14:41:07 +0100

    Update add.c

--- add.c
+++ add.c
@@ -1,22 +1,21 @@
 #include "monty.h"
-
 /**
- * f_add - Adds the top two elements of the stack.
- * @head: Stack head
- * @counter: Line number
- * Return: No return value
- */
+ * f_add - adds the top two elements of the stack.
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+*/
 void f_add(stack_t **head, unsigned int counter)
 {
 	stack_t *h;
 	int len = 0, aux;
 
 	h = *head;
-	for (; h != NULL; h = h->next)
+	while (h)
 	{
+		h = h->next;
 		len++;
 	}
-
 	if (len < 2)
 	{
 		fprintf(stderr, "L%d: can't add, stack too short\n", counter);
@@ -25,7 +24,6 @@ void f_add(stack_t **head, unsigned int counter)
 		free_stack(*head);
 		exit(EXIT_FAILURE);
 	}
-
 	h = *head;
 	aux = h->n + h->next->n;
 	h->next->n = aux;
