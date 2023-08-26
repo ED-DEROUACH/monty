a47f60e735f6752243ad3732e741cd475bfd58ab
ED-DEROUACH <97236123+ED-DEROUACH@users.noreply.github.com>
2023-08-26 23:17:19 +0100

    Update pall.c

--- pall.c
+++ pall.c
@@ -3,15 +3,14 @@
 /**
  * f_pall - prints the stack
  * @head: stack head
- * @counter: not used
+ * @counter: line_number
  * Return: no return
  */
 void f_pall(stack_t **head, unsigned int counter)
 {
-    stack_t *h;
-    (void)counter;
+    stack_t *h = *head;
+    (void) counter;
 
-    h = *head;
     while (h)
     {
         printf("%d\n", h->n);
